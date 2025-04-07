01592f86b7666ec918a870809f51e5dd3b5c0cd7
m1cha <michail.ostasov@gmail.com>
2025-04-07 13:18:11 +0300

    change to tabs

--- Assets/_Scripts/BrickStats.cs
+++ Assets/_Scripts/BrickStats.cs
@@ -3,12 +3,12 @@ using UnityEngine.Serialization;
 
 namespace BrickBuilder
 {
-    public class BrickStats : MonoBehaviour
-    {
-        //Use this to set the hieght of individual bricks.  This helps when placing bricks.
-        [FormerlySerializedAs("brickHeight")] [Tooltip("Plates = .0032 | Regular = .0096 | 2xRegular = .0192 | 3xRegular = .0288 | 4xRegular = .0384")]
-        public float _brickHeight = 0.0032f;
-        
-        public float GetBrickHeight() => _brickHeight;
-    }
+	public class BrickStats : MonoBehaviour
+	{
+		[FormerlySerializedAs("brickHeight")]
+		[Tooltip("Plates = .0032 | Regular = .0096 | 2xRegular = .0192 | 3xRegular = .0288 | 4xRegular = .0384")]
+		public float _brickHeight = 0.0032f;
+		
+		public float GetBrickHeight() => _brickHeight;
+	}
 }
