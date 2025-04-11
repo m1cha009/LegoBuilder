b5f8fbd9eb4f2752305fab53b7bd4a6bc19229de
m1cha <michail.ostasov@gmail.com>
2025-04-11 20:32:45 +0300

    add load logic

--- Assets/_Scripts/BrickData.cs
+++ Assets/_Scripts/BrickData.cs
@@ -1,14 +1,21 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace BrickBuilder
 {
 	[Serializable]
-	public class BrickData
+	public struct BrickData
 	{
 		public string prefabPath;
 		public Vector3 position;
 		public Quaternion rotation;
 		
+	}
+
+	[Serializable]
+	public class SaveData
+	{
+		public List<BrickData> BricksDataList = new();
 	}
 }
--- Assets/_Scripts/SaveLoadSystem.cs
+++ Assets/_Scripts/SaveLoadSystem.cs
@@ -19,10 +19,12 @@ namespace BrickBuilder
 			}
 			
 			
-			SaveMockup();
+			// SaveMockup();
+			
+			LoadMockup();
 		}
 
-		public void Save(string saveName, bool obfuscate, BrickData bricksList)
+		public void Save(string saveName, bool obfuscate, SaveData bricksList)
 		{
 			var json = JsonUtility.ToJson(bricksList, true);
 			if (obfuscate)
@@ -36,27 +38,17 @@ namespace BrickBuilder
 			Debug.Log($"Structure saved to {fullPath}");
 		}
 
-		public void Load()
+		public SaveData Load(string saveName, bool isObfuscated)
 		{
-			
-		}
-
-		private void SaveMockup()
-		{
-			var bricksList = new List<BrickData>();
-
-			var brickData = new BrickData()
+			var json = File.ReadAllText(Path.Combine(_savePath, $"{saveName}.json"));
+			if (isObfuscated)
 			{
-				prefabPath = "asdasd",
-				position = new Vector3(2, 3, 6),
-				rotation = Quaternion.identity
-			};
+				json = DeobfuscateJson(json);
+			}
 			
-			bricksList.Add(brickData);
-			
-			Save("we4", false, brickData);
+			return JsonUtility.FromJson<SaveData>(json);
 		}
-		
+
 		private string ObfuscateJson(string jsonString)
 		{
 			var jsonBytes = System.Text.Encoding.UTF8.GetBytes(jsonString);
@@ -67,6 +59,42 @@ namespace BrickBuilder
 		{
 			var obfuscatedBytes = Convert.FromBase64String(obfuscatedString);
 			return System.Text.Encoding.UTF8.GetString(obfuscatedBytes);
+		}
+		
+		private void SaveMockup()
+		{
+			var brickData = new BrickData()
+			{
+				prefabPath = "asdasd",
+				position = new Vector3(2, 3, 6),
+				rotation = Quaternion.identity
+			};
+			
+			var brickData2 = new BrickData()
+			{
+				prefabPath = "viva",
+				position = new Vector3(1, 2, 3),
+				rotation = Quaternion.identity
+			};
+
+
+			var saveData = new SaveData();
+			saveData.BricksDataList.Add(brickData);
+			saveData.BricksDataList.Add(brickData2);
+			
+			Save("we1", false, saveData);
+		}
+		
+		private void LoadMockup()
+		{
+			var saveData = Load("we1", false);
+
+			foreach (var brickData in saveData.BricksDataList)
+			{
+				Debug.Log($"PrefabPath: {brickData.prefabPath}");
+				Debug.Log($"Position: {brickData.position}");
+				Debug.Log($"Rotation: {brickData.rotation}");
+			}
 		}
 	}
 }
